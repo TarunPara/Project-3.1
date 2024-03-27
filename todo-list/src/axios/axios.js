f0f6a4a729fe42d9a0b80d43ba9c7b892b029f61
TarunPara <Tarunparajuli@gmail.com>
2024-03-28 04:20:03 +0500

    updated

--- todo-list/src/axios/axios.js
+++ todo-list/src/axios/axios.js
@@ -4,7 +4,8 @@ const cookies = new Cookies();
 const user = cookies.get("user");
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/v1",
+  // baseURL: "http://localhost:5000/v1",
+  baseURL: "https://project-3-1.onrender.com/v1",
   headers: { Authorization: `Bearer ${user?.tokens?.access?.token}` },
 });
 export default API;
