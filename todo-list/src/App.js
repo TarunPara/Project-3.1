74511b8f7b5c27221a519f15b74398e090064efb
TarunPara <Tarunparajuli@gmail.com>
2024-03-27 07:33:39 +0500

    register button added

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,13 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './Components/Navbar';
-import Task from './Components/Task';
-import Footer from './Components/Footer';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import Task from "./Components/Task";
+import Footer from "./Components/Footer";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div >
+    <div>
       <Navbar />
+      
       <Task />
       <Footer />
     </div>
